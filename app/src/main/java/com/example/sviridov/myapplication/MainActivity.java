ab33e551722da056d4591b6b7a409b29cb5b43fd
Sviridov <SvyrydovArtem@gmail.com>
2016-11-26 17:00:57 +0200

    increment decrement

--- app/src/main/java/com/example/sviridov/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sviridov/myapplication/MainActivity.java
@@ -1,19 +1,25 @@
 /**
  * Add your package below. Package name can be found in the project's AndroidManifest.xml file.
  * This is the package name our example uses:
- *
+ * <p>
  * package com.example.android.justjava;
  */
 package com.example.sviridov.myapplication;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    int numberOfCoffees = 1;
+    int price = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +31,27 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        int numberOfCoffees = 2;
+        int quantity = 5;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * quantity);
+    }
+
+    public void increment(View view) {
+        numberOfCoffees++;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * price);
+    }
+
+    public void decrement(View view) {
+        numberOfCoffees--;
+        if (numberOfCoffees <=0) {
+            numberOfCoffees = 0;
+            Button decreaseButton = (Button) findViewById(R.id.decrement);
+            decreaseButton.setEnabled(false);
+        }
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * price);
     }
 
     /**
@@ -34,5 +60,10 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
